b7f7c19f36c0377226eb427de606f4e8df73892f
elik <elik@example.com>
2023-02-04 20:08:25 +0300

    04.02 Night Update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-const TarifsCard = document.querySelectorAll('section-tarifs__card');
-const PromotionImage = document.querySelector(“.section-promotion__image”);
-TarifsCard.onclick=function(){
-    window.location.href = '../html';
-};
+// const TarifsCard = document.querySelectorAll('section-tarifs__card');
+const PromotionImage = document.querySelector('.section-promotion__image')
+// TarifsCard.onclick=function(){
+//     window.location.href = '../html';
+// };
+const TarifsCard = document.querySelector('.section-tarifs__card')
 PromotionImage.onclick = function(){
     PromotionImage.classList.toggle("max-size")
 }
 
+TarifsCard.onclick = function(){
+    window.location.href = "../html/tarrifs.html"
+}
