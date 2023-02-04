a2af96c30a10e0e01c8f1a75a6bf6f6f7b49719d
elik <elik@example.com>
2023-02-04 19:19:32 +0300

    04.02 AfterNoon Update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-const TarifsCard = document.querySelector(.section-tarifs__card)
+const TarifsCard = document.querySelectorAll('section-tarifs__card');
+const PromotionImage = document.querySelector(“.section-promotion__image”);
 TarifsCard.onclick=function(){
     window.location.href = '../html';
-    alert
 };
+PromotionImage.onclick = function(){
+    PromotionImage.classList.toggle("max-size")
+}
+
