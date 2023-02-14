d6a3719b152ba06b68ccb2be96f105540fa0129a
elik <elik@example.com>
2023-02-14 17:40:12 +0300

    PreFinal Release 3.3.7

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ const PromotionImage = document.querySelector('.section-promotion__image')
 // TarifsCard.onclick=function(){
 //     window.location.href = '../html';
 // };
-
+const CallToHelp = document.querySelector('.supheader-right__flex')
 const TransferToTariffs = document.querySelector('.add_more')
 const PromoteFlex = document.querySelector('section-promotion')
 
 TransferToTariffs.onclick = function(){
     window.location.href = "html/tarrifs.html"
+}
+CallToHelp.onclick = function(){
+    window.location.href = "tel: +79991234567"
 }
